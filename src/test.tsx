8b4540e6fcb8f1f803d98eadb80a6faa65a6ca4d
JaiiR320 <jair3202@gmail.com>
2025-04-14 15:16:06 -0400

    Hide cached searches
    When query changes, old cached search results still show as their data is not null, so the check passes. Now also checks if the query is allowed.

--- src/test.tsx
+++ src/test.tsx
@@ -1,59 +1,28 @@
 import { createRoot } from "react-dom/client";
-import { Stack } from "@mui/material";
-import { MiniMapProps, ScatterPlot } from "./components/ScatterPlot";
+import { Box, Typography } from "@mui/material";
+import { GenomeSearch, ResultType } from "./components/Autocomplete";
 import { useEffect, useState } from "react";
 
-type Point = {
-  x: number;
-  y: number;
-  color: string;
-  shape: "circle" | "triangle";
-};
-
-// Example data for the scatter plot
-const scatterData: Point[] = [
-  { x: 1, y: 2, color: 'red', shape: "circle" },
-  { x: 3, y: 4, color: 'blue', shape: "circle" },
-  { x: 5, y: 6, color: 'green', shape: "circle" },
-];
-
-// Mock for the map prop
-const miniMap: MiniMapProps = {
-  position: { right: 50, bottom: 50 }
-};
-
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-useEffect(() => {
-    const fakeLoading = setTimeout(() => {
-        setIsLoading(false);
-    }, 2000); // Adjust delay as needed
-
-    return () => clearTimeout(fakeLoading); // Cleanup on unmount
-}, []);
-
+  const [query, setQuery] = useState<ResultType[]>(["Gene"]);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setQuery(["SNP"]);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <Stack height={"57vh"} width={"70vw"} padding={1} sx={{ border: '2px solid', borderColor: 'grey.400', borderRadius: '8px' }}>
-      <ScatterPlot
-        pointData={scatterData}
-        loading={isLoading}
-        leftAxisLabel="sdgc"
-        bottomAxisLabel="s.khdcvsvb"
-        miniMap={miniMap}
-        disableTooltip
-        initialState={
-          {
-            minimap: {
-              open: true,
-            },
-            controls: {
-              selectionType: "pan"
-            }
-          }
-        }
+    <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" height="50vh" width="100%">
+      <Typography variant="h1">{query.join(", ")}</Typography>
+      <GenomeSearch
+        assembly="GRCh38"
+        queries={query}
+        onSearchSubmit={(result) => {
+          console.log(result);
+        }}
+        sx={{ width: "400px" }}
       />
-    </Stack>
+    </Box>
   );
 }
 
