19a5731f3c842a3b2fd8992a703f29845f2ed1fa
mlacadie <lacadiem14@gmail.com>
2025-04-24 11:29:49 -0400

    Update test.tsx

--- src/test.tsx
+++ src/test.tsx
@@ -1,26 +1,48 @@
 import { createRoot } from "react-dom/client";
-import { Box } from "@mui/material";
-import { GenomeSearch } from "./components/Autocomplete";
+import { Stack } from "@mui/material";
+import { MiniMapProps, ScatterPlot } from "./components/ScatterPlot";
+
+type Point = {
+  x: number;
+  y: number;
+  color: string;
+  shape: "circle" | "triangle";
+};
+
+// Example data for the scatter plot
+const scatterData: Point[] = [
+  { x: 1, y: 2, color: 'red', shape: "circle" },
+  { x: 3, y: 4, color: 'blue', shape: "circle" },
+  { x: 5, y: 6, color: 'green', shape: "circle" },
+];
+
+// Mock for the map prop
+const miniMap: MiniMapProps = {
+  position: { right: 50, bottom: 50 }
+};
 
 function App() {
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      height="50vh"
-      width="100%"
-    >
-      <GenomeSearch
-        assembly="GRCh38"
-        queries={["Gene", "iCRE", "cCRE"]}
-        onSearchSubmit={(result) => {
-          console.log(result);
-        }}
-        sx={{ width: "400px" }}
+    <Stack height={"57vh"} width={"70vw"} padding={1} sx={{ border: '2px solid', borderColor: 'grey.400', borderRadius: '8px' }}>
+      <ScatterPlot
+        pointData={scatterData}
+        loading={false}
+        leftAxisLabel="sdgc"
+        bottomAxisLabel="s.khdcvsvb"
+        miniMap={miniMap}
+        disableTooltip
+        initialState={
+          {
+            minimap: {
+              open: true,
+            },
+            controls: {
+              selectionType: "pan"
+            }
+          }
+        }
       />
-    </Box>
+      </Stack>
   );
 }
 
