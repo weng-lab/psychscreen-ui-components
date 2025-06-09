a69b165c8ad554306f1d72003f93b68f29403889
mlacadie <93140416+mlacadie@users.noreply.github.com>
2025-06-09 15:32:34 +0000

    Deploying to gh-pages from  @ f0ca94580f622d8dbc9dd22010bea75524834aad 🚀

---
+++ assets/DropDownMenu.stories-toEF_YW6.js
@@ -0,0 +1,8 @@
+import{j as e}from"./jsx-runtime-CDt2p4po.js";import{D as m,M as r}from"./Graph-Dr3YgECm.js";import"./scatterplot-DOCenyG3.js";import"./Autocomplete-CL-ytkHN.js";import"./violinPlot-CrIJkFj9.js";/* empty css            */import"./index-GiUgBvb1.js";import"./Grow-T4q3YgyY.js";import"./Box-tXOfBd2H.js";import"./index-CROobee-.js";import"./Typography-Bu_rT7YP.js";import"./useParentSize-ZnyO9Jw1.js";const h={title:"DropDownMenu",component:m,tags:["autodocs"],argTypes:{children:{control:{type:"text"}}},parameters:{controls:{expanded:!0}}},t={render:()=>e.jsxs(m,{children:[e.jsx(r,{children:"Disease/Trait"}),e.jsx(r,{children:"Gene/b-cCRE"}),e.jsx(r,{children:"SNP/QTL"}),e.jsx(r,{children:"Single-Cell"})]})};var n,o,s;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: () => <DropDownMenu>
+      <MenuItem>Disease/Trait</MenuItem>
+      <MenuItem>Gene/b-cCRE</MenuItem>
+      <MenuItem>SNP/QTL</MenuItem>
+      <MenuItem>Single-Cell</MenuItem>
+    </DropDownMenu>
+}`,...(s=(o=t.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const f=["Default"];export{t as Default,f as __namedExportsOrder,h as default};
