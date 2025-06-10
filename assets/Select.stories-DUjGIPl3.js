20527f183ef598c0e984f8f73ed6e323a21d77ad
mlacadie <93140416+mlacadie@users.noreply.github.com>
2025-06-10 15:57:07 +0000

    Deploying to gh-pages from  @ 7640181536e72e58683d0529e6572a44e9972375 🚀

---
+++ assets/Select.stories-DUjGIPl3.js
@@ -0,0 +1,9 @@
+import{j as e}from"./jsx-runtime-CDt2p4po.js";import{b as l,M as t}from"./Graph-D7T6PYII.js";import"./scatterplot-CcFg2cKb.js";import"./Autocomplete-CL-ytkHN.js";import"./violinPlot-CGqZlRo3.js";/* empty css            */import"./index-GiUgBvb1.js";import"./Grow-T4q3YgyY.js";import"./Box-tXOfBd2H.js";import"./index-CROobee-.js";import"./Typography-Bu_rT7YP.js";import"./useParentSize-1UGC14_K.js";const S={title:"Select",component:l,tags:["autodocs"],parameters:{controls:{expanded:!0}}},r={args:{},render:()=>e.jsxs(l,{variant:"standard",defaultValue:"disease/trait",children:[e.jsx(t,{value:"disease/trait",children:"Disease/Trait"}),e.jsx(t,{value:"gene/bcre",children:"Gene/b-cCRE"}),e.jsx(t,{value:"snp/qtl",children:"SNP/QTL"}),e.jsx(t,{value:"single-cell",children:"Single Cell"})]})};var a,s,n;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {},
+  render: () => <Select variant="standard" defaultValue="disease/trait">
+      <MenuItem value="disease/trait">Disease/Trait</MenuItem>
+      <MenuItem value="gene/bcre">Gene/b-cCRE</MenuItem>
+      <MenuItem value="snp/qtl">SNP/QTL</MenuItem>
+      <MenuItem value="single-cell">Single Cell</MenuItem>
+    </Select>
+}`,...(n=(s=r.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,S as default};
